c73cde830cd538d8edc04f0d29c3bed021095e02
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-23 13:28:05 +0200

    Scoring System

--- PongClone/Assets/Scenes/Script/CollissionControler.cs
+++ PongClone/Assets/Scenes/Script/CollissionControler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CollissionControler : MonoBehaviour
 {
     public BallMovement ballMovement;
-
+    public ScoreControler scoreController;
     void BounceFromRacket(Collision2D c)
     {
         Vector3 ballPosition = this.transform.position;
@@ -35,17 +35,19 @@ public class CollissionControler : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == "LEFTRight" || collision.gameObject.name == "RIGHTLeft")
+        if(collision.gameObject.name == "LEFTRight" || collision.gameObject.name == "LEFT" || collision.gameObject.name == "LEFTLeft" || collision.gameObject.name == "RIGHTLeft"  || collision.gameObject.name == "RIGHT" || collision.gameObject.name == "RIGHTRight")
         {
             this.BounceFromRacket(collision);
 
         }else if(collision.gameObject.name == "WallLEFT")
         {
             Debug.Log("Collision with WallLEFT");
+            this.scoreController.GoalPlayer2();
 
         }else if(collision.gameObject.name == "WallRIGHT")
         {
             Debug.Log("Collision with WallRIGHT");
+            this.scoreController.GoalPlayer1();
         }
     }
 
