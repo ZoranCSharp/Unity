b3c1b0badd937fd71f186f1367bdcff58a04ef43
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-23 11:43:45 +0200

    Racket Movement

---
+++ PongClone/Assets/Scenes/Script/RacketRight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketRight : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private void FixedUpdate()
+    {
+        float v2 = Input.GetAxisRaw("Vertical2");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v2) * movementSpeed;
+
+    }
+}
