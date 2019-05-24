47b125b0d705a8658b626c3920f45f01a0c5434c
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-24 12:16:47 +0200

    Raycast

---
+++ Practice/Assets/CheckForObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForObjects : MonoBehaviour
+{
+  
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit2;
+
+        if(Physics.Raycast(transform.position, -Vector3.up, out hit, 100f)){
+            print($"We hit something at {hit.transform.position}");
+        }
+        else
+        {
+            print("nothing underneath us");
+        }
+
+        if(Physics.Raycast(ray, 100))
+        {
+            print("hit something with mouse");
+        }
+
+        if (Physics.Raycast(ray, out hit2, 100))
+        {
+            print("hit something with mouse");
+            print($"hit {hit.collider.gameObject.name}");
+        }
+    }
+}
