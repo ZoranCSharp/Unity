2fb68de0b80b7fcfcf5bd4efc1f9e0ffb8ae3927
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-24 11:26:52 +0200

    Invoke and InvokeRepeating

--- Practice/Assets/InstantiateCubes.cs
+++ Practice/Assets/InstantiateCubes.cs
@@ -17,6 +17,16 @@ public class InstantiateCubes : MonoBehaviour
     //    }
     //}
 
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("CreateNewCube", 3f, 1f);
+
+        Invoke("CreateNewCube", 5f);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -26,5 +36,17 @@ public class InstantiateCubes : MonoBehaviour
             counter++;
         }
     
+    }
+
+
+    public void CreateNewCube()
+    {
+        Instantiate(prefab, new Vector3(-10 + counter * 3.0F, 0, 0), Quaternion.identity);
+        counter++;
+
+        if(counter >= 5)
+        {
+            CancelInvoke();
+        }
     }
 }
