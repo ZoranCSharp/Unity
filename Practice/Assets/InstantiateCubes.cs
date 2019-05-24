3326fb5ad9999056e412a4c78cab6959b575ca04
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-05-24 10:53:23 +0200

    Instatiating

---
+++ Practice/Assets/InstantiateCubes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public Transform prefab;
+
+    int counter = 0;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    for(int i =0; i< 10; i++)
+    //    {
+    //        Instantiate(prefab, new Vector3(-15 + i * 3.0F, 0,0), Quaternion.identity);
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, new Vector3(-15 + counter * 3.0F, 0, 0), Quaternion.identity);
+            counter++;
+        }
+    
+    }
+}
